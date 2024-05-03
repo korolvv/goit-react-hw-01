0f7a02b79c7fcbfeebc59e7d16d27bd2478fcfb9
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2024-05-03 16:39:50 +0300

    completed task #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import css from "./App.module.css";
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const { name, tag, location, image, stats } = userData;
+	return (
+		<>
+			<ul className={css.list}>
+				<li className={css.item}>
+					<Profile
+						name={name}
+						tag={tag}
+						location={location}
+						image={image}
+						stats={stats}
+					/>
+				</li>
+			</ul>
+		</>
+	);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+import css from "./Profile.module.css";
+
+export default function Profile({
+	name,
+	tag,
+	location,
+	image,
+	stats: { followers, views, likes },
+}) {
+	return (
+		<>
+			<div className={css.wrapper}>
+				<div className={css.img_wrapper}>
+					<img className={css.avatar} src={image} alt="User avatar" />
+				</div>
+				<p className={css.name}>{name}</p>
+				<p className={css.tag}>@{tag}</p>
+				<p>{location}</p>
+			</div>
+
+			<ul className={css.stats}>
+				<li className={css.stats_item}>
+					<span>Followers</span>
+					<span>{followers}</span>
+				</li>
+				<li className={css.stats_item}>
+					<span>Views</span>
+					<span>{views}</span>
+				</li>
+				<li className={css.stats_item}>
+					<span>Likes</span>
+					<span>{likes}</span>
+				</li>
+			</ul>
+		</>
+	);
+}
